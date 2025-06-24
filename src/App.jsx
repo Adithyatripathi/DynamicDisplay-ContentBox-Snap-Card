ff38838e897507ac671d0d5e9327a6892caa83ed
Adithyatripathi <148991165+Adithyatripathi@users.noreply.github.com>
2025-06-24 09:50:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./Header.jsx";
-import Footer from "./Footert.jsx";
+import Footer from "./Footer.jsx"; // Fixed typo
 import Content from "./Contenttemp.jsx";
 import ExtraCards from "./ExtraCards.jsx";
 
@@ -7,21 +7,16 @@ function App() {
   return (
     <>
       <Header />
-
-     
       <ExtraCards />
-
-      
       <div className="content-container flex justify-center items-center p-6">
         <Content
           name="Pikachu"
           para="An Electric-type Pokémon, full of energy!"
-          age={90} 
-          Gender={true}
-          isgood={true} 
+          age={10}
+          gender={true}
+          isGood={true}
         />
       </div>
-
       <Footer />
     </>
   );
