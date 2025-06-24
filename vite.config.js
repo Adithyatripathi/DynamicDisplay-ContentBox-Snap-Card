4b648648b3c87b199f3ec08a2db1a6d869c691e6
Adithyatripathi <adithyatripathi1279@gmail.com>
2025-06-24 15:05:25 +0530

    Added base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/DynamicDisplay-ContentBox-Snap-Card/',  // 👈 Add this line
   plugins: [react()],
 })
