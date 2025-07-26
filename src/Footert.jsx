3d99a4968cae711ec9bb389b5a3df593fee6c3a4
Adithyatripathi <adithyatripathi1279@gmail.com>
2025-07-26 13:54:28 +0530

    Initial commit of React project

--- src/Footert.jsx
+++ src/Footert.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <h1 className="text-xl font-semibold mb-2">
-        This is the end of the world, check this out!!
+        Create your won Imagination, check this out!!
       </h1>
       <p className="text-sm mb-2">
         @Adithya Tripathi; &copy; {new Date().getFullYear()} - You are ready to do it
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/DynamicDisplay-ContentBox-Snap-Card/',  // 👈 Add this line
+  
   plugins: [react()],
+  base: '/DynamicDisplay-ContentBox-Snap-Card', 
 })
